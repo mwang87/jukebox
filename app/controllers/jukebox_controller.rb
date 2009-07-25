9916b529d75657bfce5236bb13a43fdfc465a01c
mingxun <mingxun@mingxun-laptop.(none)>
2009-07-25 00:35:27 -0700

    removing scrobbler

--- app/controllers/jukebox_controller.rb
+++ app/controllers/jukebox_controller.rb
@@ -1,7 +1,7 @@
 class JukeboxController < ApplicationController
 before_filter :login_required
 require 'rubygems'
-require 'scrobbler2'
+#require 'scrobbler2'
 #require 'gingerhendrix-scrobbler2'
   def index
 #	Scrobbler2::Base.api_key = "02e45d64712a174bf95fc76b4b9b67dc"	
