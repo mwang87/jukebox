b321e316a4ce257bef7bfde606b04c64e652f2d1
mingxun <mingxun@mingxun-laptop.(none)>
2009-07-25 21:00:53 -0700

    added last fm tag stuff

--- app/controllers/jukebox_controller.rb
+++ app/controllers/jukebox_controller.rb
@@ -1,10 +1,21 @@
 class JukeboxController < ApplicationController
 before_filter :login_required
 require 'rubygems'
-require 'scrobbler2'
+require 'httparty'
+require 'json'
 #require 'gingerhendrix-scrobbler2'
   def index
-	Scrobbler2::Base.api_key = "02e45d64712a174bf95fc76b4b9b67dc"	
+	response = getTopTags
+	@tags = JSON.parse(response.body)["toptags"]["tag"]
   end
 
+  def getTopTags
+      api_key = "02e45d64712a174bf95fc76b4b9b67dc"	
+      method = "tag.getTopTags"
+      query = {}
+      options = {}
+       query = query.merge({:api_key => api_key, :method => method, :format => "json"})
+      options = options.merge({:query => query})
+      response = HTTParty.get('http://ws.audioscrobbler.com/2.0/', options)
+  end
 end
